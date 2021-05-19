e88c380165415475204d62507c5e26f3f9f0d97c
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-05-18 21:26:09 -0300

    test: should return true when CPF is valid

--- tests/entities/enrollment/cpf.test.ts
+++ tests/entities/enrollment/cpf.test.ts
@@ -2,14 +2,19 @@ import Cpf from '@/entities/enrollment/cpf';
 
 describe('CPF object value', () => {
   describe('validate', () => {
-    test('should return false when cpf have equal numbers', () => {
+    test('should return false when CPF have equal numbers', () => {
       const cpf = '00000000000';
       expect(Cpf.validate(cpf)).toBe(false);
     });
 
-    test('should return false when cpf is invalid', () => {
+    test('should return false when CPF is invalid', () => {
       const cpf = '86446422799';
       expect(Cpf.validate(cpf)).toBe(false);
+    });
+
+    test('should return true when CPF is valid', () => {
+      expect(Cpf.validate('86446422784')).toBe(true);
+      expect(Cpf.validate('864.464.227-84')).toBe(true);
     });
   });
 });
